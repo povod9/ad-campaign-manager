2466af76326aa64a6722b7a7c81411112b7979ba
povod9 <nazarpovod113@gmail.com>
2026-05-24 20:44:35 +0200

    feat: implement authorization by Jwt token

--- src/main/java/com/povod9/adcampaign/security/AuthenticationFilter.java
+++ src/main/java/com/povod9/adcampaign/security/AuthenticationFilter.java
@@ -1,4 +1,51 @@
 package com.povod9.adcampaign.security;
 
-public class AuthenticationFilter {
+import com.povod9.adcampaign.dto.PrincipalDto;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtCore jwtCore;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        try {
+            String token = authHeader.substring(7);
+
+            jwtCore.validateJwtTokenOrThrow(token);
+            Long id = jwtCore.extractId(token);
+            String email = jwtCore.extractEmail(token);
+
+            PrincipalDto principalDto = new PrincipalDto(id,email);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(principalDto, null, null);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }catch (Exception e){
+            SecurityContextHolder.clearContext();
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        filterChain.doFilter(request,response);
+
+    }
 }
--- src/main/java/com/povod9/adcampaign/security/WebSecurityConfig.java
+++ src/main/java/com/povod9/adcampaign/security/WebSecurityConfig.java
@@ -1,4 +1,39 @@
 package com.povod9.adcampaign.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http, AuthenticationFilter authenticationFilter){
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers(
+                                        "/swagger-ui.html",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/v3/api-docs.yaml",
+                                        "/swagger-resources/**",
+                                        "/webjars/**",
+                                        "/sellers/login",
+                                        "/sellers/registration"
+                                ).permitAll()
+                                .anyRequest()
+                                .authenticated()
+                );
+
+        return http.build();
+    }
 }
